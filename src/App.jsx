caf46d461e25fd7bd3eabd1c1648ce9c104074e2
dchang74 <NELSONC924@GMAIL.COM>
2023-06-22 18:17:41 -0400

    added background dim and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import Modal from './Modal'
+import CardModal from './CardModal'
 
 function App() {
   let data = [
     {
-      "name":"Nelson Chang",
+      "title":"First Title",
       "img":"https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      "name":"Bianca Chang",
+      "title":"First Title",
       "img":"https://images.pexels.com/photos/4215102/pexels-photo-4215102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
     {
-      "name":"Daniel Chang",
+      "title":"First Title",
       "img":"https://images.pexels.com/photos/7919366/pexels-photo-7919366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     },
   ]
@@ -20,7 +20,8 @@ function App() {
     <div className='card-container'>
         {data.map((item,idx) => {
             return (
-                <Modal item={item} key={idx} idx={idx} />
+                <CardModal item={item} key={idx} idx={idx} />
+                // unique card ID Must be sent 
             )
         })}
     </div>
